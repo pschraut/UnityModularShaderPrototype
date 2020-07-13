efdf454e03c481eaf05405dcd97908a6a2b48fff
pschraut <p-github@console-dev.de>
2020-07-13 14:45:21 +0200

    Fixed that shaders don't get registered to appear in the material shader dropdown.

---
+++ Editor/SurfaceShaderAssetPostprocessor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// Registers user created shader, which is required in order for those
+/// shader to appear in the material shader dropdown list.
+/// </summary>
+class SurfaceShaderAssetPostProcessor : AssetPostprocessor
+{
+    static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+    {
+        RegisterShaders(importedAssets);
+    }
+
+    static void RegisterShaders(string[] paths)
+    {
+        foreach (var assetPath in paths)
+        {
+            if (!assetPath.EndsWith(SurfaceShaderImporter.k_FileExtension, StringComparison.InvariantCultureIgnoreCase))
+                continue;
+
+            var mainObj = AssetDatabase.LoadMainAssetAtPath(assetPath) as Shader;
+            if (mainObj != null)
+                ShaderUtil.RegisterShader(mainObj);
+
+            foreach (var obj in AssetDatabase.LoadAllAssetRepresentationsAtPath(assetPath))
+            {
+                if (obj is Shader)
+                    ShaderUtil.RegisterShader((Shader)obj);
+            }
+        }
+    }
+}
